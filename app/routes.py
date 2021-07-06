a67015d1b4bcf3be93afcb103936f65e49a68ac4
WG <wgrandoms@gmail.com>
2021-07-06 16:24:07 -0400

    Replaced hello world with some mockup user posts on the home page

--- app/routes.py
+++ app/routes.py
@@ -8,5 +8,11 @@ def index():
     """This function implements the view logic for the index page."""
 
     username = 'baiber'
+    posts = [
+        {'body': 'This is the first post!',
+         'author': 'baiber'},
+        {'body': 'This web app is about investment research!',
+         'author': 'peipei'}
+    ]
 
-    return render_template('index.html', title='Home', username=username)
+    return render_template('index.html', title='Home', username=username, posts=posts)

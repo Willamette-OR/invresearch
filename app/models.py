e9b5ebb38c23775dc6dd5e87bc4447de3a3767f7
WG <wgrandoms@gmail.com>
2021-07-10 17:12:34 -0400

    Added Post as a table and ran db migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db 
 
 
@@ -11,8 +12,26 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         """This function defines the string repr of user objects."""
 
         return "<User: {}>".format(self.username)
+
+
+class Post(db.Model):
+    """
+    This class implements a table for storing post data for app users,
+    derived from SQLAlchemy's Model class.
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        """This function defines the string repr of post objects."""
+
+        return "<Post: {}>".format(self.body)

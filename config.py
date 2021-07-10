f4d135fd1933efe289d398c687beb2c8a9d538d9
WG <wgrandoms@gmail.com>
2021-07-10 16:55:08 -0400

    Fixed bugs in initializing SALAlchemy for the app

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """This class defines configuration variables for the app."""
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-hard-to-guess-so-do-not-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL') or 'sqlite://' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+        'this-is-hard-to-guess-so-do-not-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

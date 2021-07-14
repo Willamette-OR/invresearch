17c9d01062e392029349fe7e105087edacf61664
WG <wgrandoms@gmail.com>
2021-07-13 22:31:12 -0400

    Implemented logic to login and logout users

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_required, current_user, login_user, logout_user
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     """This function implements the view logic for the index page."""
 
@@ -23,9 +27,28 @@ def index():
 def login():
     """This function implements the view logic for the login page."""
 
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash("Login successful! Username - {}, Password - {}, Remember_me - {}.".format(form.username.data, form.password.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
 
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user or not user.check_password(form.password.data):
+            flash("Incorrect username or password. Please try again.")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash("You have logged in successfully!")
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            return redirect(url_for('index'))
+        return redirect(next_page)
+        
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """This function implements the logic to log out users."""
+
+    logout_user()
+    return redirect(url_for('login'))

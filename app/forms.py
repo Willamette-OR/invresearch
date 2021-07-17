e41b93d2815c5c77e60d749d1502e736c71c92c6
wei-laptop <gongwei619@hotmail.com>
2021-07-16 22:00:16 -0400

    Added a user registration page.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -10,3 +11,27 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    """This class defines the user registration form, derived from FlaskForm."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        """This method validates if the username already exists."""
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Please use a different username.")
+
+    def validate_email(self, email):
+        """This method validates if the email address has already been registered."""
+
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Please use a different email address.")
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_required, current_user, login_user, logout_user
 from werkzeug.urls import url_parse
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -52,3 +52,21 @@ def logout():
 
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """This function implements the view logic to register new users."""
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You have registered successfully!")
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)

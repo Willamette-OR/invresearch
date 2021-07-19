12199b823f748d431b2188b7b823d7c422aeb1a2
wei-laptop <gongwei619@hotmail.com>
2021-07-18 21:49:50 -0400

    Updated the style of posts and fixed a bug in user avatars

--- app/models.py
+++ app/models.py
@@ -43,7 +43,7 @@ class User(UserMixin, db.Model):
         """
 
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&?s={}'.format(
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
                 digest, size)
 
 

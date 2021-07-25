2759ecd5c0a4e0039eed93e2871c00877e58432d
wei-laptop <gongwei619@hotmail.com>
2021-07-25 18:20:16 -0400

    Fixed a bug in the form for editing user profiles

--- app/forms.py
+++ app/forms.py
@@ -45,3 +45,10 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About Me', validators=[Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        """This method validates if the submitted username is already in use."""
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Please use a different username.")

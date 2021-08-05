9413f947613a987c4d928911b339eab5bfc928d3
wei-laptop <gongwei619@hotmail.com>
2021-08-04 22:07:21 -0400

    Added pagination for user posts on the explore page

--- app/routes.py
+++ app/routes.py
@@ -182,6 +182,13 @@ def unfollow(username):
 def explore():
     """This view function handles requests to explore all user posts."""
 
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
 
-    return render_template('index.html', title='Explore', posts=posts)
+    return render_template('index.html', title='Explore', posts=posts.items, 
+                           next_url=next_url, prev_url=prev_url)

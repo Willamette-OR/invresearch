cc09b8a8d80aed5e441fcd5cc0c9b74c56f797df
wei-laptop <gongwei619@hotmail.com>
2021-08-06 15:08:37 -0400

    Added a page for users to request password reset emails

--- app/forms.py
+++ app/forms.py
@@ -72,3 +72,13 @@ class SubmitPostForm(FlaskForm):
     post = TextAreaField(
         'Say something', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    """
+    This class implements a form for users to request password reset emails, 
+    derived from FlaskForm
+    """
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')

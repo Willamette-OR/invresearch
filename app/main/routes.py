423733605b915e2a605da68334b04c75c3d9c7ac
wei-laptop <gongwei619@hotmail.com>
2021-08-22 14:31:36 -0400

    Added a page to view received private messages

--- app/main/routes.py
+++ app/main/routes.py
@@ -240,3 +240,25 @@ def send_message(recipient):
 
     return render_template('send_message.html', title='Send Message', 
                            recipient=recipient, form=form)
+
+
+@bp.route('/messages')
+@login_required
+def messages():
+    """This view function handles requests to view received messages."""
+
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.received_messages.order_by(
+        Message.timestamp.desc()).paginate(
+            page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.messages', page=messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) \
+        if messages.has_prev else None
+
+    return render_template('messages.html', title='Messages', 
+                           messages=messages.items, next_url=next_url, 
+                           prev_url=prev_url)

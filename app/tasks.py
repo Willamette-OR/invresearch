7be1972bbca38ee707dd1ced8ef0e2682744ee94
wei-laptop <gongwei619@hotmail.com>
2021-08-26 22:22:43 -0400

    Added a tasks module with a example function for Redis RQ testing

---
+++ app/tasks.py
@@ -0,0 +1,23 @@
+from time import sleep
+from rq import get_current_job
+
+
+def example(seconds):
+    """
+    This function implements a simple example for the purpose of testing Redis 
+    RQ.
+    """
+
+    job = get_current_job()
+    print("Starting a new task...")
+
+    for i in range(seconds):
+        job.meta['progress'] = i * 100.0 / seconds
+        job.save_meta()
+        print(i)
+        sleep(1)
+
+    job.meta['progress'] = 100.0
+    job.save_meta()
+    print("Task completed.")
+        

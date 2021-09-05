c2816e97a72fdbc7f57fed54faef251d0554c037
wei-laptop <gongwei619@hotmail.com>
2021-09-05 15:56:25 -0400

    Added a symbol search function in the stocks module

--- app/stocks.py
+++ app/stocks.py
@@ -1,6 +1,39 @@
 from flask import current_app
 
 
+def symbol_search(query, page, stocks_per_page):
+    """
+    This function searches for matching stocks given the input query string.
+
+    It returns a list of results given the specified page # and number per 
+    page.
+
+    It currently uses the symbol_lookup API from Finnhub.
+
+    TODO - think of ways to cache the search results for pagination, to 
+    minimize the # of API requests when users paging through search results.
+    """
+
+    # check if the api client has been configured
+    if not current_app.finnhub_client:
+        return None, 0
+
+    # perform the search via an API
+    response = current_app.finnhub_client.symbol_lookup(query)
+    
+    # prepare the search results
+    total = response['count']
+    if not isinstance(page, int) or page < 1:
+        raise ValueError("Invalidate page value.")
+    elif (page - 1) * stocks_per_page < total:
+        matched_symbols = response['result'][
+            ((page - 1) * stocks_per_page):min(page * stocks_per_page, total)]
+    else:
+        matched_symbols = None
+        
+    return matched_symbols, total
+
+
 def company_profile(symbol):
     """
     This function get company info for a given symbol.

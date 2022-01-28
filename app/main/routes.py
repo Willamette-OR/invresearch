b5486125e5c3c76a454f5afb46355ff97c639fc1
wei-laptop <gongwei619@hotmail.com>
2022-01-27 22:35:27 -0500

    Integrated posting with the stock profile page

--- app/main/routes.py
+++ app/main/routes.py
@@ -35,7 +35,8 @@ def index():
     """This function implements the view logic for the index page."""
 
     page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().filter(Post.parent==None).paginate(
+    posts = current_user.followed_posts().filter(
+        Post.parent==None).filter(Post.stock==None).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.index', page=posts.next_num) \
         if posts.has_next else None
@@ -182,9 +183,9 @@ def explore():
     """This view function handles requests to explore all user posts."""
 
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.filter(Post.parent==None).order_by(
-        Post.timestamp.desc()).paginate(
-            page, current_app.config['POSTS_PER_PAGE'], False)
+    posts = Post.query.filter(Post.parent==None).filter(Post.stock==None).\
+        order_by(Post.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.explore', page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
